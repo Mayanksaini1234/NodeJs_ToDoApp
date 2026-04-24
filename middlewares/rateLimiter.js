aae34b4f642e4a84dfaa45096915a7074317eef2
Mayank Saini <mayanksaini9389@gmail.com>
2026-04-24 23:52:42 +0530

    update rate limits

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 export const globalLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 min
-  max: 10, // max 100 requests
+  max: 200, // max 200 requests
   handler: (req, res) => {
     return res.status(429).json({
       success: false,
