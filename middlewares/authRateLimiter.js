7e7f2abdb084407786a9f8d30f8364a40600149c
Mayank Saini <mayanksaini9389@gmail.com>
2026-04-24 21:06:04 +0530

    all

--- middlewares/authRateLimiter.js
+++ middlewares/authRateLimiter.js
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 export const authLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 min
-  max: 5, // only 5 attempts
+  max: 100, // only 5 attempts
 
   handler: (req, res) => {
     return res.status(429).json({
