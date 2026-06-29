693164ac5632ea5ebafd4e1d8b2c7a212289098a
Mayank Saini <mayanksaini9389@gmail.com>
2026-06-29 23:52:53 +0530

    update sendEmial.js

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,13 +1,15 @@
 import nodemailer from "nodemailer";
 
 const sendEmail = async (to, subject, text) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail", 
-    auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS,
-    },
-  });
+ const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
 
   await transporter.sendMail({
     from: process.env.SMTP_USER,
