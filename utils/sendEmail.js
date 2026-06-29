df937aea0c4ee4948ee935f7458fe50432ee79fc
Mayank Saini <mayanksaini9389@gmail.com>
2026-06-30 00:26:20 +0530

    updated controller.js

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,24 +1,28 @@
-import nodemailer from "nodemailer";
-
+// import nodemailer from "nodemailer";
+import { Resend } from "resend";
+const resend = new Resend(process.env.RESEND_API_KEY); 
+//  const transporter = nodemailer.createTransport({
+  //   host: "smtp.gmail.com",
+  //   port: 587,
+  //   secure: false,
+  //   requireTLS: true,
+  //   auth: {
+    //     user: process.env.SMTP_USER,
+    //     pass: process.env.SMTP_PASS,
+    //   },
+    //   connectionTimeout: 60000,
+    // });
+   
+    // await transporter.sendMail({
+    //   from: process.env.SMTP_USER,
+    //   to,
+    //   subject,
+    //   text,
+    // });
+  
 const sendEmail = async (to, subject, text) => {
- const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  requireTLS: true,
-  auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
-  },
-  connectionTimeout: 60000,
-});
-console.log("SMTP_USER =", process.env.SMTP_USER);
-console.log("SMTP_PASS exists =", !!process.env.SMTP_PASS);
-await transporter.verify();
-console.log("SMTP VERIFIED");
-
-  await transporter.sendMail({
-    from: process.env.SMTP_USER,
+  await resend.emails.send({
+    from: "onboarding@resend.dev",
     to,
     subject,
     text,
